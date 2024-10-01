efe282a65017ac473c3b4c5b1f0c66db967f046c
raphaelsegalla <raphael.segalla@gmail.com>
2024-10-01 20:52:26 -0300

    first commit

---
+++ src/main/java/com/br/crypto_management_api/CryptoManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.crypto_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/crypto_management_api/CryptoManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.crypto_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
